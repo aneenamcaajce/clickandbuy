0141a96dc122a8f5ccbd8b0a8b583143cc121900
aneenamcaajce <aneenaannamanuel@mca.ajce.in>
2017-09-24 23:22:59 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>CLICK AND BUY</title>
+<link rel="stylesheet" href="coe.css" type="text/css">
+<style>
+
+ul {
+    list-style-type: none;
+    margin: 5;
+    padding: 0;
+    overflow: hidden;
+    background-color: #2441C7;
+    top: 0;
+    width: 100%;
+}
+
+li {
+    float: right;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+li a:hover {
+    background-color:#000; 
+}
+div.header{
+text-align:center;
+font-family:serif;
+height:100px;
+color:green;
+background-color:white;
+margin-top:0px;
+}
+
+#heading{
+position:absolute;
+top:10px;
+right:20px;
+width:100%;
+}
+
+
+.button{
+	width:130px;
+	height:30px;
+	background-color:#4CAF50;
+	border-radius:13px;
+	cursor: pointer;
+}
+.login{
+	border:black;
+	height : 400px;
+	width: 250px;
+	position: absolute;
+	left:1000px;
+	top:168px;
+	color:blue;
+}
+	.login7{
+	border:black;
+	height : 400px;
+	width: 650px;
+	position: absolute;
+	rightt:1000px;
+	top:168px;
+	color:blue;
+}
+	.login6{
+	border:black;
+	height : 400px;
+	width: 250px;
+	position: absolute;
+	right:900px;
+	top:200px;
+	color:blue;
+}
+.footer{
+	position:absolute;
+	top:800px;
+	width:100%;
+	color:white;
+	background-color:#060001;
+	height:150px;
+}
+.imagegalllery1{
+	position:absolute;
+	top:168px;
+	width:100%;
+	height:300px;
+}
+.imagegalllery{
+	position:absolute;
+	top:600px;
+	width:100%;
+	height:300px;
+}
+.img{
+	position:absolute;
+	border: 3px solid black;
+	width:900px;
+	height:400px;
+	top:168px;
+}
+.login_pic{
+    width: 30%;
+    border-radius: 50%;
+}
+.login_image_container{
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-right:16px;
+    padding-left: 16px;
+}
+div.main{
+	top:570px;
+	left:500px;
+	border:.10px solid black;
+	width:700px;
+	height:400px
+}
+div.main1{
+	float:right;
+	position:absolute;
+	top:570px;
+	left:560px;
+	border:.5px solid black;
+	width:400px;
+	height:400px
+}
+</style>
+</head>
+<body>
+<div class="header">
+<h1 id="heading" align="left">&nbsp&nbsp&nbsp&nbsp<img src="photo1.jpg" border="0" id="LogoImage" alt="Herb Affair" width="130" height="70">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i>CLICK AND BUY</i></h1>
+</div>
+
+<ul style="margin-top:2px;">
+<li><a class="active" href="registration.php">REGISTER</a></li>
+<li><a class="active" href="login1.php">LOGIN</a></li>
+ <li><a class="active" href="#home">OFFERS</a></li>
+ <li><a class="active" href="#home">HOME</a></li>
+</ul>
+
+<div class=main>
+<h1>Todays exicting offers</h1>
+<img src="jkl.jpg" width="650">
+</div>
+<div class=main1>
+
+</div>
+<form action="logcon.php" method="post">
+<table align="center" cellpadding="4px">
+</table></br>
+</form>
+
+</div>
+
+<footer><div class="footer">
+            <table  align="center" cellpadding="10px">
+                <tr>
+                    <td width="33%">
+                        <img src="img2.jpg" alt="icon6" width="80px" height="80px"/>
+                    </td>
+                    <td width="33%">
+                        <img src="img4.jpg" alt="icon7" width="80px" height="80px"/> 
+                    </td>
+                    <td width="33%">
+                        <img src="img6.jpg" alt="icon8" width="80px" height="80px"/>
+                    </td>
+                </tr>
+            </table>
+            <i style="position:relative;top:20px;left:320px;">@2016 Amal Jyothi College of Engineering.All rights Reserved. Powered by Department of MCA 
+| <a href="#main">Top</a></i>
+        </div></footer> 
+</body>
+</html>
